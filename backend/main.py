fc577dff288c095559352d3223afccdbd4b986c8
kshitij-hash <kjangra1557@gmail.com>
2025-04-16 21:43:07 +0530

    chore: cors fix

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend origin
+    allow_origins=["https://dock-ai-frontend.vercel.app"],  # Frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
